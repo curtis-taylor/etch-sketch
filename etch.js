c58de154b4b8c794a18a695245c01bb263c31b4f
curtis taylor <curtis.t@gmail.com>
2025-09-27 03:23:06 -0400

    add javascript to html

--- etch.js
+++ etch.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.grid_container');
 
-const square = document.createElement('');
-square.setAttribute("id", "01");
+const square = container.createElement('');
+square.setAttribute(".square", "ss");
 
