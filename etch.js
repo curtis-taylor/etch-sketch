b310fa773371fce91cb37d8c8a8149032429aaa1
curtis taylor <curtis.t@gmail.com>
2025-09-30 03:11:24 -0400

    trying to make more than 1 squarec

--- etch.js
+++ etch.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('#grid_container');
 
-const square = document.createElement('div');
-square.setAttribute("id", "square");
+
 
 
 function square_test() {
+    let square = document.createElement('div');
+    square.setAttribute("class", "square");
+    container.append(square);
     container.append(square);
     console.log('square')
 }
