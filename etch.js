95fa973be10c312364bda28693b79872fa887bdf
curtis taylor <curtis.t@gmail.com>
2025-10-05 03:12:07 -0400

    Got slider value change to work. Trying to add square number function

--- etch.js
+++ etch.js
@@ -1,20 +1,22 @@
 
 
 
-function square_test() {
+function square_test(square_length) {
 
     const container = document.querySelector('#grid_container');
    
     if(container.children.length < 1) {
     
 
-        for (let i = 0; i < 256; i++) {
+        for (let i = 0; i < (square_length * square_length); i++) {
             let square = document.createElement('div');
             square.setAttribute("class", "square");
             square.setAttribute("id", i);
+            square.style.height = "calc(100% / square_length)"
+            square.style.flex = "0 0 calc(100% / square_length)"
             container.appendChild(square);
 
-            console.log('square')
+            console.log('square ' + square_length * square_length);
         }
 
         
