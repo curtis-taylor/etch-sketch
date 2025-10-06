1dafb471030fc2e2894a30443b1ebcef477023e2
curtis-taylor <curtis.t@gmail.com>
2025-10-05 22:10:51 -0400

    Got grid to display mostly depending on most numbers Needs more work

--- etch.js
+++ etch.js
@@ -12,11 +12,23 @@ function square_test(square_length) {
             let square = document.createElement('div');
             square.setAttribute("class", "square");
             square.setAttribute("id", i);
-            square.style.height = "calc(100% / square_length)"
-            square.style.flex = "0 0 calc(100% / square_length)"
+            /*
+            square.setAttribute("style", "height: calc(100% / square_length); flex: 0 0 calc(100% / square_length; ")
+            square.setAttribute("style", "flex: 0 0 calc(100% / square_length;")
+            square.setAttribute("style", "background-color: white;  border: 1px solid #999; box-sizing: border-box;")
+            */
+
+            //square.style.setProperty('--grid-size', square_length);
+            //square.style.flex = "0 0 calc(100% / square_length)"
+            let txt = "--grid-size: " + square_length + ";";
+            square.setAttribute("style", txt);
             container.appendChild(square);
 
+            let computedStyles = window.getComputedStyle(square);
             console.log('square ' + square_length * square_length);
+            console.log( computedStyles.height);
+            console.log(computedStyles.backgroundColor);
+            console.log(txt)
         }
 
         
@@ -27,7 +39,14 @@ function square_test(square_length) {
             e.preventDefault();
         //e.stopPropagation();
         e.stopImmediatePropagation();
-        console.log(e);
+        //console.log(e);
+        console.log("****");
+
+        let computedStyles2 = window.getComputedStyle(item);
+
+        console.log(computedStyles2.height);
+        let txt = "--grid-size: " + square_length + " ;";
+        item.setAttribute('style', txt);
         item.setAttribute('style', 'background-color: red;');
         })
     })
